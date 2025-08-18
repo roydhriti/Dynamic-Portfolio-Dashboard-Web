cc51f39df009b01500e988da1767a6379144eeaa
dhirtiroy321 <dhriti@ovntech.com>
2025-08-18 17:59:02 +0530

    first commit

--- src/service/stock-service.tsx
+++ src/service/stock-service.tsx
@@ -1,7 +1,7 @@
 import { apiClient } from "../config/apiClient";
 
 export const fetchStockDataAPI = (symbol: any): Promise<any> =>
-  apiClient<any>(`/stocks/api/stock-data?symbol=${symbol}`, "GET");
+  apiClient<any>(`/portfolio/stock-price?symbol=${symbol}`, "GET");
 
 export const fetchPortfolioStockAPI = (): Promise<any> =>
-  apiClient<any>("/stocks/api/portfolio-stocks/", "GET");
+  apiClient<any>("/portfolio/stock-price/", "GET");
