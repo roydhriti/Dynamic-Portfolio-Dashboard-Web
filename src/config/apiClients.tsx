031ff032c604da4f7c51bff6e3eac7e61712766a
dhirtiroy321 <dhriti@ovntech.com>
2025-08-20 11:32:15 +0530

    done

--- src/config/apiClients.tsx
+++ src/config/apiClients.tsx
@@ -1,5 +1,7 @@
 
 const API_BASE_URL = "http://127.0.0.1:3000";
+// const API_BASE_URL = "https://dynamic-porfolioapi-2.onrender.com";
+
 
 
 if (!API_BASE_URL) {
--- src/service/stock-service.tsx
+++ src/service/stock-service.tsx
@@ -1,7 +1,16 @@
 import { apiClient } from "../config/apiClient";
 
+// export const fetchStockDataAPI = (symbol: any): Promise<any> =>
+//   apiClient<any>(`/stocks/api/stock-data?symbol=${symbol}`, "GET");
+
 export const fetchStockDataAPI = (symbol: any): Promise<any> =>
   apiClient<any>(`/portfolio/stock-price?symbol=${symbol}`, "GET");
 
 export const fetchPortfolioStockAPI = (): Promise<any> =>
   apiClient<any>("/portfolio/stock-price/", "GET");
+
+// export const fetchPortfolioStockAPI = (): Promise<any> =>
+//   apiClient<any>("/stocks/api/stock-data/", "GET");
+
+
+// portfolio / stock - price
