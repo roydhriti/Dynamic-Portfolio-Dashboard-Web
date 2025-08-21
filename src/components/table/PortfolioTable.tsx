a715bfc87847ebf488aa9ecb0a596da07e031a0a
dhirtiroy321 <dhriti@ovntech.com>
2025-08-21 17:04:50 +0530

    fix

--- src/components/table/PortfolioTable.tsx
+++ src/components/table/PortfolioTable.tsx
@@ -50,7 +50,7 @@ const PortfolioTable = ({ portfolio }: { portfolio: any[] }) => {
 
     const interval = setInterval(fetchData, 15000);
     return () => clearInterval(interval);
-  }, []);
+  }, [fetchData]);
 
   const columns = React.useMemo(() => getColumns(), []);
   const tableInstance = useTable({ columns, data: rows });
